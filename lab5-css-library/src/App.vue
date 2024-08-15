58a0a6625ee3ea3dccd78d403ae1dd6534c67070
Jireh Mae Muyana <jirehmae_m@cmu.ac.th>
2024-08-16 02:11:48 +0700

    until 10.3
    not a checkpoint

--- lab5-css-library/src/App.vue
+++ lab5-css-library/src/App.vue
@@ -10,18 +10,18 @@ const { message } = storeToRefs(store)
   <!-- <div class="bg-blue-500 text-white p-4">
     <h1 class="text-2xl font-bold text-center">it's working </h1>
   </div> -->
-  <div class="text-center font-sans text-gray-700 antialiased">
+  <div class="text-center font-sans text-gray-700 antialias">
     <header>
-      <div id="flashMessage" v-if="message">
+      <div id="flashMessage" class="animate-fade" v-if="message">
         <h4>{{ message }}</h4>
       </div>
       <div class="wrapper">
-        <nav>
-          <RouterLink :to="{ name: 'event-list-view' }">Event</RouterLink>
-          <span class="separator"> | </span>
-          <RouterLink :to="{ name: 'about' }">About</RouterLink>
-          <span class="separator"> | </span>
-          <RouterLink :to="{ name: 'student-info' }">Student Information</RouterLink>
+        <nav class="py-6">
+          <RouterLink class="font-bold text-gray-700" exact-active-class="text-green-500" :to="{ name: 'event-list-view' }">Event</RouterLink>
+          |
+          <RouterLink class="font-bold text-gray-700" exact-active-class="text-green-500" :to="{ name: 'about' }">About</RouterLink>
+          |
+          <RouterLink class="font-bold text-gray-700" exact-active-class="text-green-500" :to="{ name: 'student-info' }">Student Information</RouterLink>
         </nav>
       </div>
     </header>
@@ -30,7 +30,7 @@ const { message } = storeToRefs(store)
 </template>
 
 <style>
-nav {
+/* nav {
   padding: 30px;
 }
 
@@ -40,12 +40,12 @@ nav a {
 }
 nav a.router-link-exact-active {
   color: #42b983;
-}
+} */
 
 h2 {
   font-size: 20px;
 }
-@keyframes yellofade {
+/* @keyframes yellofade {
   from {
     background-color: yellow;
   }
@@ -55,9 +55,5 @@ h2 {
 }
 #flashMessage {
   animation: yellofade 3s ease-in-out;
-}
-.separator {
-  color: #2c3e50;
-  font-weight: bold;
-}
+} */
 </style>
