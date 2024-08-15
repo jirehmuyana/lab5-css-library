457f433d8bd2f2945718befec21e871311fb7827
Jireh Mae Muyana <jirehmae_m@cmu.ac.th>
2024-08-16 01:11:51 +0700

    2.3

--- lab5-css-library/src/views/EventListView.vue
+++ lab5-css-library/src/views/EventListView.vue
@@ -57,7 +57,7 @@ onMounted(() => {
   <h1>Events For Good</h1>
   <!-- new element -->
 
-  <div class="events">
+  <div class="flex flex-col items-center">
     <EventCard v-for="event in events" :key="event.id" :event="event" />
     <!-- <EventDetails v-for="event in events" :key="event.id" :event="event" /> -->
     <div class="pagination">
@@ -81,11 +81,6 @@ onMounted(() => {
 </template>
 
 <style scoped>
-.events {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
 .pagination {
   display: flex;
   width: 290px;
