9cebe554583e41f1b199bb82df5329a9082321f3
Jireh Mae Muyana <jirehmae_m@cmu.ac.th>
2024-08-16 02:22:04 +0700

    11.1

--- lab5-css-library/src/App.vue
+++ lab5-css-library/src/App.vue
@@ -15,8 +15,8 @@ const { message } = storeToRefs(store)
       <div id="flashMessage" class="animate-fade" v-if="message">
         <h4>{{ message }}</h4>
       </div>
-      <div class="wrapper">
-        <nav class="py-6">
+      <div class="container mx-auto px-4 py-6">
+        <nav class="py-8">
           <RouterLink class="font-bold text-gray-700" exact-active-class="text-green-500" :to="{ name: 'event-list-view' }">Event</RouterLink>
           |
           <RouterLink class="font-bold text-gray-700" exact-active-class="text-green-500" :to="{ name: 'about' }">About</RouterLink>
@@ -28,32 +28,3 @@ const { message } = storeToRefs(store)
     <RouterView />
   </div>
 </template>
-
-<style>
-/* nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-nav a.router-link-exact-active {
-  color: #42b983;
-} */
-
-h2 {
-  font-size: 20px;
-}
-/* @keyframes yellofade {
-  from {
-    background-color: yellow;
-  }
-  to {
-    background-color: transparent;
-  }
-}
-#flashMessage {
-  animation: yellofade 3s ease-in-out;
-} */
-</style>
